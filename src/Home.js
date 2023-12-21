986e09213bad008d6b54fb10936f1f6a202682c2
aishU3010 <153792530+aishU3010@users.noreply.github.com>
2023-12-21 09:48:23 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,11 @@ import bank from '../src/images/bank.webp';
 export default function Home() {
   return (
     <div >
-   <br/> <center> <h2>BAD BANK</h2></center> <br/>
+   <br/> <center> <h2>AXIAL BANK</h2></center> <br/>
  
    <div className="marquee-container">
       <div className="marquee">
-      <h4>Welcome to BAD BANK - Deposite , Withdraw , Borrow effortlessly, Repay flexibly... 
+      <h4>Welcome to AXIAL BANK - Deposite , Withdraw , Borrow effortlessly, Repay flexibly... 
 Get upto ₹5 lakh and repay flexibly across 12 months. No interest charged if paid in one month!</h4>
       </div>
     </div>
@@ -16,7 +16,7 @@ Get upto ₹5 lakh and repay flexibly across 12 months. No interest charged if p
     <Card style={{ width: '50%' }}>
       <Card.Img variant="top" src={bank} height={500} width={500} alt='bankimage'/>
       <Card.Body>
-        <Card.Title>BAD BANK</Card.Title>
+        <Card.Title>AXIAL BANK</Card.Title>
         <Card.Text>
         “Everyday is a bank account, and the time is our currency. No one is rich, no one is poor, we’ve got 24 hours each.”
         </Card.Text>
